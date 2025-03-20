f32ab3c47e84d46ea01c1a2b70ecfc68816d5e62
Alex <alex.white017@gmail.com>
2025-03-20 13:28:20 -0500

    Changed tables

--- src/Details.jsx
+++ src/Details.jsx
@@ -28,8 +28,8 @@ const Details = () => {
             </h1>
           </div>
 
-          <div className="pokedex-data-container flex">
-            <div className="image-container flex justify-center items-center">
+          <div className="pokedex-data-container flex align-top">
+            <div className="image-container ">
               <img
                 src={
                   pokemonData.sprites?.other["official-artwork"].front_default
@@ -38,7 +38,7 @@ const Details = () => {
                 className=" h-auto"
               />
             </div>
-            <table className="basic-data table-auto w-2/3 border-collapse">
+            <table className="basic-data table-auto w-2/3 border-collapse mx-5">
               <thead>
                 <tr>
                   <th className="px-4 py-2 text-center" colSpan="2">
@@ -98,7 +98,7 @@ const Details = () => {
                 </tr>
               </tbody>
             </table>
-            <table className="training-data table-auto w-2/3 border-collapse border-gray-300 my-4">
+            <table className="training-data table-auto w-2/3 border-collapse border-gray-300">
               <thead>
                 <tr>
                   <th className="px-4 py-1 text-center" colSpan={2}>
@@ -145,123 +145,135 @@ const Details = () => {
             </table>
           </div>
 
-          <div className="stats-container flex flex-col justify-start items-center">
+          <div className="stats-container flex flex-col justify-start items-center my-10">
             <h2 className="text-xl font-bold">Stats</h2>
             <ul>
               {pokemonData.stats?.map((stat) => {
                 return (
                   <li key={stat.stat.name}>
-                    <span className="font-bold">{stat.stat.name}</span>:{" "}
-                    {stat.base_stat}
+                    <span className="font-bold text-transform: capitalize">
+                      {stat.stat.name}
+                    </span>
+                    : {stat.base_stat}
                   </li>
                 );
               })}
             </ul>
           </div>
 
-          <div className="moves-container flex flex-col justify-center items-center">
-            <h2 className="text-xl font-bold">Moves</h2>
-            <table className="moves-table table-auto w-2/3 border-collapse border-gray-300 my-4">
-              <thead>
-                <tr>
-                  <th className="px-4 py-1 text-center">Level</th>
-                  <th className="px-4 py-1 text-center">Move</th>
-                </tr>
-              </thead>
-              <tbody>
-                {pokemonData.moves
-                  ?.filter(
-                    (move) =>
-                      move.version_group_details[0].move_learn_method.name ===
-                      "level-up"
-                  )
-                  .sort(
-                    (move1, move2) =>
-                      move1.version_group_details[0].level_learned_at -
-                      move2.version_group_details[0].level_learned_at
-                  )
-                  .map((move) => (
-                    <tr key={move.move.name}>
-                      <td className="px-4 py-1 text-center">
-                        {move.version_group_details[0].level_learned_at}
-                      </td>
-                      <td className="px-4 py-1 text-center">
-                        {move.move.name}
-                      </td>
-                    </tr>
-                  ))}
-              </tbody>
-            </table>
+          <div className="moves-container flex w-4/5">
+            <div className="level-up flex-col w-2/3">
+              <h2 className="text-xl font-bold text-center">Level-up Moves</h2>
+              <table className="moves-table table-auto  border-collapse border-gray-300 my-4 m-auto">
+                <thead>
+                  <tr>
+                    <th className="px-4 py-1 text-center">Level</th>
+                    <th className="px-4 py-1 text-center">Move</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {pokemonData.moves
+                    ?.filter(
+                      (move) =>
+                        move.version_group_details[0].move_learn_method.name ===
+                        "level-up"
+                    )
+                    .sort(
+                      (move1, move2) =>
+                        move1.version_group_details[0].level_learned_at -
+                        move2.version_group_details[0].level_learned_at
+                    )
+                    .map((move) => (
+                      <tr className="border-t border-b" key={move.move.name}>
+                        <td className="px-4 py-1 text-center">
+                          {move.version_group_details[0].level_learned_at}
+                        </td>
+                        <td className="px-4 py-1 text-center text-transform: capitalize">
+                          {move.move.name}
+                        </td>
+                      </tr>
+                    ))}
+                </tbody>
+              </table>
+            </div>
 
-            <h2 className="text-xl font-bold">Machine Moves</h2>
-            <table className="machine-moves-table table-auto w-2/3 border-collapse border-gray-300 my-4">
-              <thead>
-                <tr>
-                  <th className="px-4 py-1 text-center">Move</th>
-                </tr>
-              </thead>
-              <tbody>
-                {pokemonData.moves
-                  ?.filter(
-                    (move) =>
-                      move.version_group_details[0].move_learn_method.name ===
-                      "machine"
-                  )
-                  .map((move) => (
-                    <tr key={move.move.name}>
-                      <td className="px-4 py-1 text-center">
-                        {move.move.name}
-                      </td>
-                    </tr>
-                  ))}
-              </tbody>
-            </table>
-            <h2 className="text-xl font-bold">Egg Moves</h2>
-            <table className="egg-moves-table table-auto w-2/3 border-collapse border-gray-300 my-4">
-              <thead>
-                <tr>
-                  <th className="px-4 py-1 text-center">Move</th>
-                </tr>
-              </thead>
-              <tbody>
-                {pokemonData.moves
-                  ?.filter(
-                    (move) =>
-                      move.version_group_details[0].move_learn_method.name ===
-                      "egg"
-                  )
-                  .map((move) => (
-                    <tr key={move.move.name}>
-                      <td className="px-4 py-1 text-center">
-                        {move.move.name}
-                      </td>
-                    </tr>
-                  ))}
-              </tbody>
-            </table>
-            <h2 className="text-xl font-bold">Tutor Moves</h2>
-            <table className="tutor-moves-table table-auto w-2/3 border-collapse border-gray-300 my-4">
-              <thead>
-                <tr>
-                  <th className="px-4 py-1 text-center">Moves</th>
-                </tr>
-              </thead>
-              <tbody>
-                {pokemonData.moves
-                  ?.filter(
-                    (move) =>
-                      move.version_group_details[0].move_learn_method.name ===
-                      "tutor"
-                  )
-                  .map((move) => (
-                    <tr key={move.move.name}>
-                      <td className="px-4 py-1 text-center">
-                        {move.move.name}
-                      </td>
-                    </tr>
-                  ))}
-              </tbody>
-            </table>
+            <div className="machine-moves-container flex-col w-2/3">
+              <h2 className="text-xl font-bold text-center">Machine Moves</h2>
+              <table className="machine-moves-table table-auto w-2/3 border-collapse border-gray-300 my-4 m-auto">
+                <thead>
+                  <tr>
+                    <th className="px-4 py-1 text-center">Move</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {pokemonData.moves
+                    ?.filter(
+                      (move) =>
+                        move.version_group_details[0].move_learn_method.name ===
+                        "machine"
+                    )
+                    .map((move) => (
+                      <tr className="border-t border-b" key={move.move.name}>
+                        <td className="px-4 py-1 text-center text-transform: capitalize">
+                          {move.move.name}
+                        </td>
+                      </tr>
+                    ))}
+                </tbody>
+              </table>
+            </div>
+            <div className="tutor-moves-container flex-col w-2/3">
+              <h2 className="text-xl font-bold text-center">Egg Moves</h2>
+              <table className="egg-moves-table table-auto w-2/3 border-collapse border-gray-300 my-4 m-auto">
+                <thead>
+                  <tr>
+                    <th className="px-4 py-1 text-center">Move</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {pokemonData.moves
+                    ?.filter(
+                      (move) =>
+                        move.version_group_details[0].move_learn_method.name ===
+                        "egg"
+                    )
+                    .map((move) => (
+                      <tr className="border-t border-b" key={move.move.name}>
+                        <td className="px-4 py-1 text-center text-transform: capitalize">
+                          {move.move.name}
+                        </td>
+                      </tr>
+                    ))}
+                </tbody>
+              </table>
+            </div>
+            <div className="tutor-moves-container flex-col w-2/3">
+              <h2 className="text-xl font-bold m-auto text-center">
+                Tutor Moves
+              </h2>
+              <table className="tutor-moves-table table-auto w-2/3 border-collapse border-gray-300 my-4 m-auto">
+                <thead>
+                  <tr>
+                    <th className="px-4 py-1 text-center">Moves</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {pokemonData.moves
+                    ?.filter(
+                      (move) =>
+                        move.version_group_details[0].move_learn_method.name ===
+                        "tutor"
+                    )
+                    .map((move) => (
+                      <tr className="border-t border-b" key={move.move.name}>
+                        <td className="px-4 py-1 text-center text-transform: capitalize">
+                          {move.move.name}
+                        </td>
+                      </tr>
+                    ))}
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
       </div>
